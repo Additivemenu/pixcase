4caceb9757c06c91e5ede99190e1dd7bf1349823
XUeshuo Li <spoonlee24k@gmail.com>
2024-05-07 21:11:04 +1000

    feat: added a phonecase img in landing page

---
+++ src/components/Phone.tsx
@@ -0,0 +1,48 @@
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import React, { HTMLAttributes } from "react";
+
+// ! good to learn extending HTMLAttributes
+interface PhoneProps extends HTMLAttributes<HTMLDivElement> {
+  imgSrc: string;
+  dark?: boolean;
+  className?: string;
+}
+
+// rest props of HTMLDivElement is in ...props
+const Phone = ({ imgSrc, className, dark = false, ...props }: PhoneProps) => {
+  return (
+    <div
+      className={cn(
+        "relative pointer-events-none z-50 overflow-hidden",
+        className
+      )}
+      {...props}
+    >
+      {/* phonecase template img */}
+      <Image
+        className="pointer-events-none z-50 select-none"
+        src={
+          dark
+            ? "/phone-template-dark-edges.png"
+            : "/phone-template-white-edges.png"
+        }
+        alt="phone image"
+        width={300}
+        height={600}
+      />
+      {/* overlaying  */}
+      <div className="absolute -z-10 inset-0">
+        <Image
+          className="object-cover"
+          src={imgSrc}
+          alt="overlaying phone image"
+          width={300}
+          height={600}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Phone;
